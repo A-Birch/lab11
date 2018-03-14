8171e562792652974a9a8eaf0d67ac1eb6758586
AlexBerezkin <mail.berezkin@gmail.com>
2018-03-13 20:14:27 -0700

    lab11
    The Login Popup Form

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 /*eslint-env browser*/
 
+function init() {
+    "use strict";
+    var login = window.document.getElementById("login");
+    login.addEventListener("click", function() {
+       window.open("login.html", "login", "width=400, height=400"); 
+    });
+   
+}
+
+window.addEventListener("load", init);
